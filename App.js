c37868eec91531128f0ba86cbe7ae7d69eee8d74
Ribamar <ribamaar@gmail.com>
2018-06-14 11:46:36 -0300

    testing prettier before commit

--- App.js
+++ App.js
@@ -10,7 +10,6 @@ const styles = StyleSheet.create({
   },
 });
 
-
 export default () => (
   <View style={styles.container}>
     <Text>Open up App.js to start working on your app!</Text>
--- App.test.js
+++ App.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import App from './App';
 
-
 it('renders without crashing', () => {
   const rendered = renderer.create(<App />).toJSON();
   expect(rendered).toBeTruthy();
